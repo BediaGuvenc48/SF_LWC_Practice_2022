438689dda56926b6424314d9a51beaef8037364d
BediaGuvenc48 <43507793+BediaGuvenc48@users.noreply.github.com>
2022-08-28 22:42:35 -0500

    Wire Adaptors Get Record

---
+++ force-app/main/default/lwc/getAccountRecord/getAccountRecord.js
@@ -0,0 +1,44 @@
+import { LightningElement, wire } from 'lwc';
+
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import TYPE_FIELD from '@salesforce/schema/Account.Type';
+
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const FIELDS=[NAME_FIELD, TYPE_FIELD, INDUSTRY_FIELDm,ANNUAL_REVENUE_FIELD];
+
+
+export default class GetAccountRecord extends LightningElement {
+
+  recordId='0018c00002SzeFTAAZ';
+
+  account='';
+
+  @wire(getRecord,{recordId:'$recordId', fields:FIELDS})
+
+  accountInfoHandler({data, error}){
+
+    if(data){
+        console.log(data);
+        this.account=data;
+    }
+
+    if(error){
+
+        console.log(error);
+    }
+
+
+
+  }
+
+
+
+
+
+
+
+
+}
